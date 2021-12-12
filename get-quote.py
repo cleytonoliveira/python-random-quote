d20bab61ef10afc92fecfb4adc2f769799fd8e28
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-12-12 16:24:15 -0300

    Printed out more than one quote

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,18 @@
 import random
 
 
-def primary():
-    file = open("quotes.txt")
-    quotes = file.readlines()
-    last = len(quotes) - 1
+def random_quote(last):
     random_quote = random.randint(0, last)
-    file.close()
+    return random_quote
 
-    print(quotes[random_quote])
+
+def primary():
+    with open("quotes.txt") as file:
+        quotes = file.readlines()
+        last = len(quotes) - 1
+
+        for i in range(2):
+            print(f'{i + 1} - {quotes[random_quote(last)]}')
 
 
 if __name__ == "__main__":
