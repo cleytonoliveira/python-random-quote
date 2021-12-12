b45f3dab9c11da7e1916dde4243a6c520af70642
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-12-12 16:59:07 -0300

    Add a new quote

--- get-quote.py
+++ get-quote.py
@@ -6,13 +6,28 @@ def random_quote(last):
     return random_quote
 
 
-def main():
-    with open("quotes.txt") as file:
-        quotes = file.readlines()
-        last = len(quotes) - 1
+def add_new_quote(quote):
+    with open("quotes.txt", "a") as file:
+        file.write(f"{quote}\n")
 
-        for i in range(2):
-            print(f'{i + 1} - {quotes[random_quote(last)].strip()}')
+
+def main():
+    try:
+        response = input("Do you want to add a new quote? ")
+        if response == "yes":
+            quote = input("Type your new quote: ")
+            add_new_quote(quote)
+
+    except KeyboardInterrupt:
+        print("\nCTRL + C exception. Please run again!")
+
+    else:
+        with open("quotes.txt") as file:
+            quotes = file.readlines()
+            last = len(quotes) - 1
+
+            for i in range(2):
+                print(f"{i + 1} - {quotes[random_quote(last)].strip()}")
 
 
 if __name__ == "__main__":
